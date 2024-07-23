f0852f50f3eff15854016c5f69a25797c8e87839
Sohail-9 <s.suhail9849@gmail.com>
2024-07-23 23:08:58 +0530

    fix:connected database

--- server/server.js
+++ server/server.js
@@ -1,14 +1,21 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
+const db = require('mongoose')
+require('dotenv').config()
 //middleware
-
-
+app.use(cors());
+//db
+db.connect(process.env.mongoUrl).then(() => {
+    console.log("connected to db")
+})
+.catch((e) => {
+    console.error("throw error" + e);
+})
 //routing
 app.get("/", (req, res) => {
     res.send("running app");
 })
-
 //listen port 
 app.listen('3000', () => {
     console.log("port running");
