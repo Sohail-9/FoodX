09622356b7fb22ed2307413f333e937bf6d9081f
Sohail-9 <s.suhail9849@gmail.com>
2024-08-01 13:41:54 +0530

    Fix : Server.JS

--- server/server.js
+++ server/server.js
@@ -2,21 +2,33 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 const db = require('mongoose')
+const parser = require("body-parser")
 require('dotenv').config()
+const {createUser, loginUser} = require("./controller/userController")
+const {createPost, getAllData} = require("./controller/postController")
+
 //middleware
 app.use(cors());
-//db
+app.use(parser.json());
+
+//database
 db.connect(process.env.mongoUrl).then(() => {
     console.log("connected to db")
 })
 .catch((e) => {
     console.error("throw error" + e);
 })
+
 //routing
 app.get("/", (req, res) => {
     res.send("running app");
 })
+app.post("/post/create", createPost);
+app.post("/user/create", createUser);
+app.get("/user/login", loginUser);
+app.get("/post/details", getAllData);
 //listen port 
-app.listen('3000', () => {
-    console.log("port running");
+const port =  process.env.PORT
+app.listen(port, () => {
+    console.log(`The server is running on http://localhost:${port}`);
 })
